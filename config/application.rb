6f2e1b15c00745a86a160bb6b5c4b186ebe2ee28
jason1110 <Jason.easterly11@gmail.com>
2021-06-21 21:19:58 -0600

    add ddtrace

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
-require 'datadog/statsd'
+require "datadog/statsd"
+require 'ddtrace/auto_instrument'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
