b64d007bf8e440eb8b4023ee31ce9d35a45d50ee
jason1110 <Jason.easterly11@gmail.com>
2021-06-22 13:14:00 -0600

    add runtime metrics

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,6 +1,10 @@
 # config/initializers/datadog.rb
+require 'datadog/statsd'
+require 'ddtrace'
+
 Datadog.configure do |c|
     c.use :rails, service_name: 'staging-rails-app'
     c.env = 'staging'
-    c.service = 'Ruby Example'  
+    c.service = 'Ruby Example'
+    c.runtime_metrics.enabled = true
 end
